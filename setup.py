857f679782b63bc6ede9eb8d4232652d08fae92e
Louis Haddrell <louishaddrell@gmail.com>
2020-07-22 11:08:22 +1200

    Fix code

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ from djangocms_column import __version__
 
 INSTALL_REQUIRES = [
     'django-cms>=3.4.5',
-    'Django>=1.11,<4.0',
+    'six>=1.9.0',
+    'Django>=1.11,<4',
 ]
 
 TESTS_REQUIRE = [
